9e39f51141b0a24e6a52bc09810026a12074a783
Richard <richard.atodo@yahoo.com>
2025-04-25 21:47:54 +0100

    Added  variables

--- eks-cluster/variables.tf
+++ eks-cluster/variables.tf
@@ -13,7 +13,32 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version for the EKS cluster"
   type        = string
-  default     = "1.31"
+  default     = "1.31" # Check AWS EKS documentation for latest supported versions
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"] # across 3 AZs
+}
+
+variable "public_subnets" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"] # across 3 AZs
+}
+
+variable "azs" {
+  description = "Availability Zones for VPC subnets"
+  type        = list(string)
+  # If default region is changed, update AZs accordingly
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 variable "node_instance_type" {
@@ -25,7 +50,7 @@ variable "node_instance_type" {
 variable "node_group_name" {
   description = "Name for the EKS managed node group"
   type        = string
-  default     = "my-ltc-eks-nodegroup"
+  default     = "ltc-eks-nodegroup"
 }
 
 variable "node_group_desired_size" {
